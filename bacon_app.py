f5d0e1cb83640da39359d4c0aa103222b275fd82
Alma <a.q.frankenstein@gmail.com>
2020-05-04 18:18:01 -0700

    add user login and db

--- bacon_app.py
+++ bacon_app.py
@@ -1,26 +1,63 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 
-from flask import Flask, render_template
-from forms import EncryptForm, DecryptForm
+# combines __init__.py and app.py files from Grinberg tutorial
+
+from flask import Flask, render_template, redirect, url_for, flash, request
+from forms import EncryptForm, DecryptForm, LoginForm
 from config import Config
 from bacon_cipher import plaintext_to_biliteral, biliteral_to_plaintext, biliteral_to_decoy, decoy_to_biliteral
 from frankenstein import frankenstein
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from forms import RegistrationFor
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
+from models import User, Message
 
-@app.route('/bacon')
+@app.shell_context_processor # when 'flask shell' command runs, it registers the return values in the shell session
+def make_shell_context():
+    return {'db': db, 'User': User, 'Message': Message}
+
+@app.route('/bacon') # = 'index' in Grinberg
+@app.route('/')
+@login_required
 def greet():
-    return 'hello bacon'
+    return render_template('greet.html')
+    #return 'hello bacon'
 
-@app.route('/encrypted/<ciphertext>')
-def show_encrypted_text():
-    return ciphertext
+# @app.route('/encrypted/<ciphertext>') # ?
+# def show_encrypted_text():
+#     return ciphertext
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('encrypt'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('wrong username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('greet')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/encrypt', methods = ['GET', 'POST'])
+@login_required
 def encrypt():
     form = EncryptForm()
     if form.is_submitted():
@@ -39,3 +76,22 @@ def decrypt():
         decrypted_text = biliteral_to_plaintext(biliteral)
         return render_template('decrypted.html', decrypted_text=decrypted_text)
     return render_template('decrypt.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('greet'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('now you\'re registered!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
--- forms.py
+++ forms.py
@@ -2,7 +2,9 @@
 # _*_ coding: utf-8 _*_
 
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, PasswordField, BooleanField, StringField
+from wtforms.validators import DataRequired, DataRequired, Email, EqualTo, InputRequired, ValidationError
+from models import User
 
 class EncryptForm(FlaskForm):
     plaintext = TextAreaField()
@@ -14,3 +16,19 @@ class DecryptForm(FlaskForm):
     plaintext = TextAreaField()
     submit = SubmitField('decrypt')
 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember me')
+    submit = SubmitField('sign in')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('sign in')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('that username\'s taken')
