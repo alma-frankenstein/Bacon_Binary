dba05ba2ff2b72098613c477825355c7b0498acb
Alma <a.q.frankenstein@gmail.com>
2020-06-27 13:11:23 -0700

    clean up comments and TODOs

--- config.py
+++ config.py
@@ -2,11 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-<<<<<<< HEAD
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'totally_secret'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-=======
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'totally_secret' # needed for flask-wtf?
->>>>>>> origin/master
